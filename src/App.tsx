d3b5edb686ba4db0b36efe5b28f3ffcdccc517bf
youkouJB <yabukabu.closer@gmail.com>
2022-07-12 21:03:29 +0900

    route setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Top from './pages/top'
+import About from './pages/about'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="text-center">
-      <h1>Hello React!</h1>
-    </div>
+    <Router>
+      <main>
+        <Routes>
+          <Route path='/' element={<Top/>} />
+          <Route path='/about' element={<About/>} />
+        </Routes>
+      </main>
+    </Router> 
   )
 }
 
