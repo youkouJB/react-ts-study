bf08b873a119d4bf4419897a731037dc04495ed0
youkouJB <yabukabu.closer@gmail.com>
2022-07-15 10:53:27 +0900

    add store

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Header from './pages/header/index';
 import Footer from './pages/footer/index'
 import Top from './pages/index';
 import About from './pages/about/index';
+import Counter from './pages/count/index';
+import Test from './container/index';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Top/>} />
           <Route path='/about' element={<About/>} />
+          <Route path='/count' element={<Counter/>} />
+          <Route path='/test' element={<Test/>} />
         </Routes>
       </main>
       <Footer />
